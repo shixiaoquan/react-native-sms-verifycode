a81bdac55ab810c0fed37539f1744ab7d93edff6
shixiaoquan <1010083274@qq.com>
2018-03-21 16:11:29 +0800

    Code formatting for eslint

--- Example/App.js
+++ Example/App.js
@@ -4,20 +4,20 @@
  * @flow
  */
 
-import React, { Component } from 'react';
+import React, { Component } from 'react'
 import {
   Platform,
   StyleSheet,
   Text,
   View
-} from 'react-native';
+} from 'react-native'
 
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' +
     'Cmd+D or shake for dev menu',
   android: 'Double tap R on your keyboard to reload,\n' +
     'Shake or press menu button for dev menu',
-});
+})
 
 type Props = {};
 export default class App extends Component<Props> {
@@ -34,7 +34,7 @@ export default class App extends Component<Props> {
           {instructions}
         </Text>
       </View>
-    );
+    )
   }
 }
 
@@ -55,4 +55,4 @@ const styles = StyleSheet.create({
     color: '#333333',
     marginBottom: 5,
   },
-});
+})
