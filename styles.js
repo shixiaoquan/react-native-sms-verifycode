5a7d9d569684893c87a51ec5c96dba73c90e6724
Stanislav <54876681+StunIsLove@users.noreply.github.com>
2021-05-24 10:27:47 +0600

    Update styles.js

--- styles.js
+++ styles.js
@@ -14,10 +14,9 @@ const styles = StyleSheet.create({
     width: getScreenWidth(),
   },
   hiddenTextInput: {
-    //width: 0,
-    //height: 0,
-    backgroundColor: 'transparent',
-    color: '#F2F2F2' // Write your background color
+    width: 1,
+    height: 1,
+    backgroundColor: 'transparent'
   },
 });
 
